b7de78af46fba33a1527eb40feb3bb3a34dc7da7
Plavala <oscar.cdbs@gmail.com>
2022-07-30 17:31:15 -0400

    added examples of works

--- src/components/pages/Blog.js
+++ src/components/pages/Blog.js
@@ -3,22 +3,22 @@ import React from 'react';
 export default function Blog() {
   return (
     <div>
-      <h1>Blog Page</h1>
-      <p>
-        Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula.
-        Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
-        dictum. Ut vel ante eget massa ornare placerat. Etiam nisl orci, finibus
-        sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum nunc,
-        sed pretium risus rutrum eget. Nam consequat, ligula in faucibus
-        vestibulum, nisi justo laoreet risus, luctus luctus mi lacus sit amet
-        libero. Class aptent taciti sociosqu ad litora torquent per conubia
-        nostra, per inceptos himenaeos. Mauris pretium condimentum tellus eget
-        lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-        Donec placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum
-        in velit pellentesque vehicula sit amet at augue. Maecenas aliquam
-        bibendum congue. Pellentesque semper, lectus non ullamcorper iaculis,
-        est ligula suscipit velit, sed bibendum turpis dui in sapien.
-      </p>
+      <h1>Examples of some of my work's!</h1>
+      <h2>Code Refactor</h2>
+      <h3>
+        Here is a project where I was given a web page with mostly working HTML and CSS files. I was tasked to clean up the HTML and CSS files, make sure all links worked, taggs where all in order and all while keeping the website looking the same as the given example. To see the repository for this project click here: <a href='https://github.com/Plavala/CBHW-W1-CodeRefactor'>Github.com/Plavala/CBHW-W1-CodeRefactor</a> 
+        </h3>
+        <img src="public\images\01-html-css-git-homework-demo.png" alt="Code Refactor" />
+      <h2>Find my Crypto</h2>
+      <h3>
+        Find My crypto is the name of my very first group project and major class project. the site is for people who need help with keeping track of crypto curency and help calculate amounts regarding all types of crypto. to check this project out please click the link! <a href='https://github.com/Plavala/CB-Project1-Find-My-Crypto'>github.com/Plavala/CB-Project1-Find-My-Crypto</a>
+        </h3>
+        <img src="public\images\Screenshot 2022-04-26 183755.png" alt="Find-my-crypto" />
+      <h2>BarCard</h2>
+      <h3>
+        BarCard is a project I worked with a group to develop. For our 2nd major project in my bootcamp we where asked to crerat a website that involved everything we had learnd so far including: front-end, back-end and server side APIs. if you click on the link below it will take you to the head of the group who holds the repo for the site. <a href='https://github.com/heatherknoyes/BarCard'>Github.com/heatherknoyes/BarCard</a>
+        </h3>
+        <img src="public\images\BarCard front page demo.png" alt="BarCard" />
     </div>
   );
 }
